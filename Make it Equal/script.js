36feb73b757be427bdc9a6d97dfdf7d413f75c08
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-10-20 19:09:38 +0200

    Modified Make it Equal (Still not working)

--- Make it Equal/script.js
+++ Make it Equal/script.js
@@ -14,13 +14,13 @@
 
 const a = {
     value: 2,
-    [Symbol.toPrimitive](hint) {
-        if (hint === 'number') {
+    [Symbol.toPrimitive](type) {
+        if (type === 'number') {
             return this.value
         }
         return this.value++
     }
 }
-// console.log(a == 2)
-// console.log(a == 3)
-console.log(a == 2 && a == 3)
+console.log(`a == 2: ${a == 2}`)
+console.log(`a == 3: ${a == 3}`)
+console.log(`a == 2 && a == 3: ${a == 2 && a == 3}`);
