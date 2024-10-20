be6e802c9010f49ae7f99361dabd6b3e5b9e88f5
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-10-20 18:36:58 +0200

    Modified the Double Every Other to add new method

--- Double Every Other/script.js
+++ Double Every Other/script.js
@@ -1,13 +1,27 @@
-
 function doubleEveryOther(array) {
-    const doubledArray = array.map((element, index) => {
-        if (index % 2 !== 0) {
-            return element * 2
-        }
-        return element
-    })
-    return doubledArray
+  const doubledArray = array.map((element, index) => {
+    if (index % 2 !== 0) {
+      return element * 2;
+    }
+    return element;
+  });
+  return doubledArray;
 }
 
-const testOne = [1,2,3,4,5,6]
-console.log(doubleEveryOther(testOne))
+// const testOne = [1, 2, 3, 4, 5, 6];
+// console.log(doubleEveryOther(testOne));
+
+function secondDoubleEveryOther(numbers = []) {
+  const newDoubledArray = numbers.map((number, index) => {
+    if (index % 2 !== 0 && index !== 0) {
+      return number * 2;
+    } else {
+      return number;
+    }
+  });
+
+  return newDoubledArray
+}
+
+const otherTest = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log("the doubled array is: ", secondDoubleEveryOther(otherTest));
