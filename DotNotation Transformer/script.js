ff8e7521647f058a60ea38151bcf91ddeaaf5c99
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-10-20 18:54:46 +0200

    Revised the DotNotation Transformer 6 KYU kata

--- DotNotation Transformer/script.js
+++ DotNotation Transformer/script.js
@@ -1,17 +1,32 @@
-function transformer(notation = '', text = '') {
-    const props = notation.split('.')
-    const resultArray = []
-    for (let prop of props) {
-        resultArray.push("{", prop, ": ")
-    }
+function transformer(notation = "", text = "") {
+  const props = notation.split(".");
+  const resultArray = [];
+  for (let prop of props) {
+    resultArray.push("{", prop, ": ");
+  }
 
-    resultArray.push("'", text, "'")
+  resultArray.push("'", text, "'");
 
-    for (let _ of props) {
-        resultArray.push("}")
-    }
+  for (let _ of props) {
+    resultArray.push("}");
+  }
 
-    console.log(resultArray.join(''))
+  console.log(resultArray.join(""));
 }
 
-transformer("kata.dot.notation", "final value")
+// transformer("kata.dot.notation", "final value")
+
+function transformDotNotation(text = "", finalValue = "") {
+    const properties = text.split('.')
+    const finalObject = []
+    for (let property of properties) {
+        finalObject.push(`{ ${property}: `)
+    }
+    finalObject.push(finalValue)
+    for (const _ of properties) {
+        finalObject.push(" }")
+    }
+
+    console.log(finalObject.join(''))
+}
+transformDotNotation("kata.dot.notation", "This Kata")
