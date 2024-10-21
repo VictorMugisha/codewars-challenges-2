3f4a7972f2c1ea3e9f4e80989c1b4d57e773543a
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-10-21 15:00:00 +0200

    Modified Array SquareUp to

--- Array SquareUp b/script.js
+++ Array SquareUp b/script.js
@@ -1,18 +1,11 @@
 function squareUp(n) {
+  let res = [];
+  for (let i = 1; i <= n; i++)
+    for (let j = n; j >= 1; j--) res.push(j <= i ? j : 0);
 
-    let res = [];
-  
-    for (let i = 1; i <= n; i++)
-      for (let j = n; j >= 1; j--)
-        res.push(j <= i ? j : 0);
-  
-    return res;
-        
-  }
+  return res;
+}
 
-
-
-
-console.log(squareUp(3)) // => [0, 0, 1, 0, 2, 1, 3, 2, 1]
-console.log(squareUp(2)) // => [0, 1, 2, 1]
-console.log(squareUp(4)) // => [0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1]
+console.log(squareUp(3)); // => [0, 0, 1, 0, 2, 1, 3, 2, 1]
+console.log(squareUp(2)); // => [0, 1, 2, 1]
+console.log(squareUp(4)); // => [0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1]
